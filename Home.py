d9a78e2bf15b12beead996acbb350b3cb0261031
AdamTaieb <104378238+AdamTaieb@users.noreply.github.com>
2023-02-24 14:51:02 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import requests
-from streamlit_lottie import st_lottie
+# from streamlit_lottie import st_lottie
 
 
 
 st.set_page_config(page_title="Clothing Retailer", page_icon="👕", layout="wide")
 
-def load_lottie(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+# def load_lottie(url):
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         return None
+#     return r.json()
 
-# Load Assets
-lottie_clothes = load_lottie("https://assets2.lottiefiles.com/packages/lf20_gn0tojcq.json")
+# # Load Assets
+# lottie_clothes = load_lottie("https://assets2.lottiefiles.com/packages/lf20_gn0tojcq.json")
 
 # Header Section
 with st.container():
@@ -42,8 +42,8 @@ with st.container():
         )
     st.write("[View Our Collection](http://localhost:8501/Shop)")
  
-    with right_column:
-        st_lottie(lottie_clothes, height=300, key="clothing")
+#     with right_column:
+#         st_lottie(lottie_clothes, height=300, key="clothing")
 
 
 # Section 4 - Latest Arrivals
